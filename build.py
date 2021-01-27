281f6e030b4a2a54ce8d211d6e199452b9053ebf
Izmar Verhage <izmaelverhage@gmail.com>
2021-01-27 23:21:33 +0100

    Add 32bit option for VS

--- build.py
+++ build.py
@@ -10,7 +10,7 @@ class MyParser(argparse.ArgumentParser):
         sys.exit(2)
 
 parser = MyParser(description='Build the vmpc2000xl workspace.')
-parser.add_argument('buildtool', help='The build tool you want to build the workspace for. Options are vs, xcode and ninja.')
+parser.add_argument('buildtool', help='The build tool you want to build the workspace for. Options are vs, vs32, xcode and ninja.')
 parser.add_argument('-o', '--offline', action='store_true', help='Offline mode. No git clone or pull and no Conan package fetching.')
 parser.add_argument('-c', '--clean', action='store_true', help='Clean all build dirs before building.')
 
@@ -31,8 +31,8 @@ def clean_folders():
     shutil.rmtree("moduru/build", ignore_errors=True)
     shutil.rmtree("build", ignore_errors=True)
 
-if args.buildtool != 'vs' and args.buildtool != 'xcode' and args.buildtool != 'ninja':
-    print('Build tool has to be vs, xcode or ninja')
+if args.buildtool != 'vs' and args.buildtool != 'vs32' and args.buildtool != 'xcode' and args.buildtool != 'ninja':
+    print('Build tool has to be vs, vs32, xcode or ninja')
     quit()
 
 if args.clean == True:
@@ -63,11 +63,13 @@ if args.offline == False:
 		run("git clone https://github.com/izzyreal/vmpc-juce")
 
 os.chdir("build")
-run("conan workspace install ../conanws.yml --build missing")
-run("conan workspace install ../conanws.yml -s build_type=Debug --build missing")
+run("conan workspace install ../conanws.yml -s arch_build=x86 --build missing")
+run("conan workspace install ../conanws.yml -s arch_build=x86 -s build_type=Debug --build missing")
 
 if args.buildtool == 'vs':
     run('cmake .. -G "Visual Studio 16 2019"')
+elif args.buildtool == 'vs32':
+    run('cmake .. -G "Visual Studio 16 2019" -A Win32')
 elif args.buildtool == 'xcode':
     run('cmake .. -G "Xcode"')
 elif args.buildtool == 'ninja':
