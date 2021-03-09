632a8a35e652c1e20388d672dd2e1d67f2bb465e
Izmar Verhage <izmaelverhage@gmail.com>
2021-03-09 21:06:14 +0100

    Fix single config builds, i.e. make

--- build.py
+++ build.py
@@ -9,8 +9,8 @@ class MyParser(argparse.ArgumentParser):
         self.print_help()
         sys.exit(2)
 
-parser = MyParser(description='Build the vmpc2000xl workspace.')
-parser.add_argument('buildtool', help='The build tool you want to build the workspace for. Options are vs, vs32, xcode and ninja.')
+parser = MyParser(description='Build the VMPC2000XL workspace.')
+parser.add_argument('buildtool', help='The build tool you want to build the workspace for.\nOptions are vs, vs32, xcode, make, ninja, ninja-multi (same as ninja) and ninja-single.')
 parser.add_argument('-o', '--offline', action='store_true', help='Offline mode. No git clone or pull and no Conan package fetching.')
 parser.add_argument('-c', '--clean', action='store_true', help='Clean all build dirs before building.')
 
@@ -31,8 +31,8 @@ def clean_folders():
     shutil.rmtree("moduru/build", ignore_errors=True)
     shutil.rmtree("build", ignore_errors=True)
 
-if args.buildtool != 'vs' and args.buildtool != 'vs32' and args.buildtool != 'xcode' and args.buildtool != 'ninja':
-    print('Build tool has to be vs, vs32, xcode or ninja')
+if args.buildtool != 'vs' and args.buildtool != 'vs32' and args.buildtool != 'xcode' and args.buildtool != 'ninja-single'and args.buildtool != 'ninja-multi'and args.buildtool != 'ninja' and args.buildtool != 'make':
+    print('Build tool has to be vs, vs32, xcode, make, ninja, ninja-multi or ninja-single')
     quit()
 
 if args.clean == True:
@@ -66,6 +66,11 @@ os.chdir("build")
 if args.buildtool == 'vs32':
 	run("conan workspace install ../conanws.yml -s arch_build=x86 --build missing")
 	run("conan workspace install ../conanws.yml -s arch_build=x86 -s build_type=Debug --build missing")
+elif args.buildtool == 'make' or args.buildtool == 'ninja-single':
+    if not os.path.exists("Debug"): os.mkdir("Debug")
+    if not os.path.exists("Release"): os.mkdir("Release")
+    run("cd Release && conan workspace install ../../conanws.yml --build missing && cd ..")
+    run("cd Debug && conan workspace install ../../conanws.yml --build missing -s build_type=Debug && cd ..")
 else:
 	run("conan workspace install ../conanws.yml --build missing")
 	run("conan workspace install ../conanws.yml --build missing -s build_type=Debug")
@@ -76,12 +81,21 @@ elif args.buildtool == 'vs32':
     run('cmake .. -G "Visual Studio 16 2019" -A Win32')
 elif args.buildtool == 'xcode':
     run('cmake .. -G "Xcode"')
-elif args.buildtool == 'ninja':
+elif args.buildtool == 'ninja' or args.buildtool == 'ninja-multi':
     run('cmake .. -G "Ninja Multi-Config"')
+elif args.buildtool == 'ninja-single':
+    run('cmake .. -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -B ./Debug')
+    run('cmake .. -G "Ninja" -DCMAKE_BUILD_TYPE=Release -B ./Release')
+elif args.buildtool == 'make':
+    run('cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -B ./Debug')
+    run('cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -B ./Release')
 
-# Uncomment the below to build an executable
+# Uncomment the below to build a target
+# When using Unix Makefiles or Ninja single config
+#run('cmake --build ./Release --config Release --target vmpc2000xl_LV2')
+
+# When using any other generator
 #run('cmake --build . --config Release --target vmpc2000xl_Standalone')
-#run('cmake --build . --config Debug --target vmpc2000xl_Standalone')
 
 # Uncommend the below to run the executables
 #os.chdir("..")
