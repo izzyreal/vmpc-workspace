2246efc99538c9434b28ec8f1e715ad156e60edc
Izmar Verhage <izmaelverhage@gmail.com>
2021-05-13 14:46:54 +0100

    Add Windows CI

---
+++ .ci/build-and-publish.bat
@@ -0,0 +1,10 @@
+cd \Users\izmar\git\vmpc-workspace
+python build.py -c vs
+cd build
+cmake --build . --config Release --target vmpc2000xl_All
+
+cd ..
+cd vmpc-workspace-32
+python build.py -c vs32
+cd build
+cmake --build . --config Release --target vmpc2000xl_All
