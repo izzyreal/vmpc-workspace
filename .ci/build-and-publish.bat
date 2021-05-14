d0761c4aabe250f6ee65a2ddc5473283cfbb9b37
Izmar Verhage <izmaelverhage@gmail.com>
2021-05-14 14:32:58 +0100

    Improve Windows CI

--- .ci/build-and-publish.bat
+++ .ci/build-and-publish.bat
@@ -9,3 +9,15 @@ cd vmpc-workspace-32
 python build.py -c vs32
 cd build
 cmake --build . --config Release --target vmpc2000xl_All
+
+cd \Users\izmar\git\vmpc-binaries
+git pull
+
+iscc \Users\izmar\git\vmpc-installer-scripts\win\vmpc.iss
+
+git add installers
+
+FOR /F "tokens=*" %a in ('get-version.cmd') do SET VERSION=%a
+
+git commit -m "Publish %VERSION% Windows"
+git push
---
+++ .ci/get-version.cmd
@@ -0,0 +1,1 @@
+PowerShell (Get-Command C:\Users\izmar\git\vmpc-workspace\vmpc-juce\build\vmpc2000xl_artefacts\Release\Standalone\VMPC2000XL.exe).FileVersionInfo.FileVersion
