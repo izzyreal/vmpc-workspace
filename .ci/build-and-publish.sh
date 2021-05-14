e42bd1100ee2aa94d51992ee466371028de987bd
Izmar Verhage <izmaelverhage@gmail.com>
2021-05-14 11:25:31 +0200

    Add MacOS publish step

--- .ci/build-and-publish.sh
+++ .ci/build-and-publish.sh
@@ -4,3 +4,10 @@ cd ~/git/vmpc-workspace
 python3 build.py -c xcode
 cd build
 cmake --build . --config Release --target vmpc2000xl_All
+
+packagesbuild ~/git/vmpc-installer-scripts/mac/VMPC2000XL.pkgproj
+
+cd ~/git/vmpc-binaries
+git add installers
+git commit -m "Publish $VERSION MacOS"
+git push
