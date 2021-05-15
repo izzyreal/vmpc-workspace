b5477489fc6df351a89e32cf77466dab1f3669eb
Buildbot <izmaelverhage@gmail.com>
2021-05-15 11:51:24 +0000

    Improve Ubuntu CI

--- .ci/build-and-publish-ubuntu.sh
+++ .ci/build-and-publish-ubuntu.sh
@@ -6,11 +6,9 @@ cd build
 ninja -f build-Release.ninja vmpc2000xl_All
 
 cd ~/git/vmpc-binaries
-git pull
 
 ./copy_bin_ubuntu18_x64.sh
 
 git add linux
 
 git commit -m "Publish 0.4 Ubuntu x64"
-git push
