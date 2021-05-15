e634f907e8c3ee6af0195bc5333defc49cb53c4a
Izmar Verhage <izmaelverhage@gmail.com>
2021-05-15 11:34:21 +0100

    Fix Windows CI

--- .ci/build-and-publish.bat
+++ .ci/build-and-publish.bat
@@ -1,14 +1,14 @@
 cd \Users\izmar\git\vmpc-workspace
-rem python build.py -c vs
+python build.py -c vs
 cd build
-rem cmake --build . --config Release --target vmpc2000xl_All
+cmake --build . --config Release --target vmpc2000xl_All
 
 cd ..
 cd ..
 cd vmpc-workspace-32
-rem python build.py -c vs32
+python build.py -c vs32
 cd build
-rem cmake --build . --config Release --target vmpc2000xl_All
+cmake --build . --config Release --target vmpc2000xl_All
 
 cd \Users\izmar\git\vmpc-binaries
 git pull
@@ -20,4 +20,5 @@ git add installers
 FOR /F "tokens=*" %%a in ('\Users\izmar\git\vmpc-workspace\.ci\get-version.cmd') do SET VERSION=%%a
 
 git commit -m "Publish %VERSION% Windows"
-git push https://user:pw@github.com/izzyreal/vmpc-binaries
+
+EXIT 0
