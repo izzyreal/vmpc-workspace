d329bc8db65f386e445a7bbcee42ab1e966ef52f
Izmar Verhage <izmaelverhage@gmail.com>
2021-05-15 12:58:51 +0200

    Improve Ubuntu CI

--- .ci/build-and-publish-ubuntu.sh
+++ .ci/build-and-publish-ubuntu.sh
@@ -4,3 +4,13 @@ cd ~/git/vmpc-workspace
 python3 build.py -c ninja
 cd build
 ninja -f build-Release.ninja vmpc2000xl_All
+
+cd ~/git/vmpc-binaries
+git pull
+
+./copy_bin_ubuntu18_x64.sh
+
+git add linux
+
+git commit -m "Publish 0.4 Ubuntu x64"
+git push
