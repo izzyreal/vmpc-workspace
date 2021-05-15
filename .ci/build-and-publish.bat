d6daf02b09b182a3e3bba386fa8c428a243da322
Izmar Verhage <izmaelverhage@gmail.com>
2021-05-15 08:43:54 +0100

    Foo

--- .ci/build-and-publish.bat
+++ .ci/build-and-publish.bat
@@ -1,14 +1,14 @@
 cd \Users\izmar\git\vmpc-workspace
-python build.py -c vs
+rem python build.py -c vs
 cd build
-cmake --build . --config Release --target vmpc2000xl_All
+rem cmake --build . --config Release --target vmpc2000xl_All
 
 cd ..
 cd ..
 cd vmpc-workspace-32
-python build.py -c vs32
+rem python build.py -c vs32
 cd build
-cmake --build . --config Release --target vmpc2000xl_All
+rem cmake --build . --config Release --target vmpc2000xl_All
 
 cd \Users\izmar\git\vmpc-binaries
 git pull
@@ -17,7 +17,7 @@ iscc \Users\izmar\git\vmpc-installer-scripts\win\vmpc.iss
 
 git add installers
 
-FOR /F "tokens=*" %a in ('get-version.cmd') do SET VERSION=%a
+FOR /F "tokens=*" %%a in ('\Users\izmar\git\vmpc-workspace\.ci\get-version.cmd') do SET VERSION=%%a
 
 git commit -m "Publish %VERSION% Windows"
-git push
+git push https://user:pw@github.com/izzyreal/vmpc-binaries
